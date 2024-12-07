426ea4799585657dfef955aecda4c6520abcbc4b
AbdullahAlamdar <ahmadalamdar73@gmail.com>
2024-12-07 12:31:28 +0500

    update traffic condition method added , vehicles loaded , vehicles located

--- q.cpp
+++ q.cpp
@@ -18,6 +18,16 @@ class ListNode{
 
 };
 
+struct vehicle{
+    string id;
+    int currentLocation;
+    int destination;
+    int shortestPath[100];
+    int pathLength;
+    int pathIndex;
+
+};
+
 class list{
     public:
     ListNode*head;
@@ -45,11 +55,13 @@ class graph{
     public:
     int vertices;
     list*ListArray;
-
+    vehicle cars[50];
+    int vehicleCount;
 
     graph(int vertices){
         this->vertices=vertices;
         ListArray=new list[vertices];
+        vehicleCount=0;
     }
 
     void addEdge(int src,int dest, int weight){
@@ -156,6 +168,80 @@ class graph{
             } 
     }
 
+    int calculatePathLength(int src, int dest){
+        // arrays 
+        int distance[vertices];
+        int parent[vertices];
+        bool visited[vertices];
+
+        // initial values
+
+        for(int i=0;i<vertices;i++){
+            distance[i]=INT8_MAX; // infinite
+            parent[i]=-1;    // no parent
+            visited[i]=false; // not visited yet
+        }
+
+        // initializing the source to source dist
+        distance[src]=0;
+
+        // main algo loop
+        for(int count=0;count<vertices-1;count++){
+            // for finding the vertex with min distance that has not yet been visited 
+            int minDistance=INT8_MAX;// infinite
+            int u=-1;
+            // 2nd loop 
+            for(int i=0;i<vertices;i++){
+                if(!visited[i] && distance[i]<minDistance){
+                    minDistance=distance[i];
+                    u=i;
+                }
+            }
+
+            visited[u]=true;
+
+            //for updating the distances of neighbouring vertices
+            ListNode*neighbour=ListArray[u].head;
+            while(neighbour!=nullptr){
+                int v=neighbour->dest;
+                int weight=neighbour->weight;
+
+                // for updating the distance if a shorter path is find
+                if (!visited[v] && distance[u] != INT8_MAX && distance[u] + weight < distance[v]) {
+                distance[v] = distance[u] + weight;
+                parent[v] = u;
+            }
+            neighbour = neighbour->next;
+            }
+            return distance[dest];
+        }
+    }
+
+    void updateTrafficCondition(int src,int dest,int newWeight){
+        // will traverse the arrayList to find the source array of the list and then update the weight of respected edge
+        ListNode*temp=ListArray[src].head;
+
+        while(temp!=nullptr){
+            if(temp->dest==dest){
+                temp->weight=newWeight;
+                cout<<"Traffic updated for edge ("<<src<<" -> "<<dest<<" ) "<<" with "<<newWeight<<endl;
+                return;
+            }
+            temp=temp->next;
+        }
+        cout<<"Entered edge ("<<src<<" -> "<<dest<<" ) , not found ! "<<endl; 
+    }
+
+    void locateVehicle(string id, int currentLocation,int destination){
+        cars[vehicleCount].currentLocation=currentLocation;
+        cars[vehicleCount].destination=destination;
+        cars[vehicleCount].id=id;
+        cars[vehicleCount].pathLength=calculatePathLength(currentLocation,destination);
+        cout<<"Shortest Path for vehicle "<<cars[vehicleCount].id;
+        graph::DijkstraAlgo(currentLocation,destination);
+        vehicleCount++;
+    }
+
 
     void displayGraph(){
         for(int i=0;i<vertices;i++){
@@ -185,7 +271,25 @@ class graph{
             addEdge(src,dest,weight);
         }
         file.close();
-    }//
+    }
+
+    void LoadVehiclesFromFile(const string& filename){
+        ifstream file(filename);
+
+        string line;
+        string V_id;
+        int startVertex,endVertex;
+        while(getline(file,line)){
+            int pos1=line.find(',');
+            int pos2=line.find(',',pos1+1);
+
+            V_id=line.substr(0,pos1);
+            startVertex=stoi(line.substr(pos1+1,pos2-pos1-1));
+            endVertex=stoi(line.substr(pos2+1));
+            locateVehicle(V_id,startVertex,endVertex);
+        }
+        file.close();
+    }
 };
 int main(){
     int vertices=5;
@@ -209,5 +313,8 @@ int main(){
     // cityGraph.displayGraph();
     // cout<<endl;
     // cout<<endl;
-    cityGraph.DijkstraAlgo(0,4);
+    //cityGraph.LoadVehiclesFromFile("vehicles.csv");
+    cityGraph.locateVehicle("v1",0,4);
+   // cityGraph.locateVehicle("v2",1,3);
+   // cityGraph.locateVehicle("v3",0,2);
 }
